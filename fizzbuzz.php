d171b8d74228deb1bd5fe7fa71b10624e5cbb528
ryuki-apex <doragonknight212@gmail.com>
2021-08-10 17:32:38 +0900

    fizzbuzz test

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,19 +1,20 @@
 <?php
 
-$i=0;
+$i = 0;
 while ($i < 100) {
+    //15->'fizzbuzz' 5->'buzz' 3->'fizz'
     // もし$iが3で割り切れた場合'fizz'と表示してください 
-  (++$i);
-
-    if ($i % 3===0){
-    // $i % 3 == 0;
-       echo("fizz");
-     
-    
-    }else { echo($i);
-    // $i % 3 != 0;
- 
+   (++$i);
+    if ($i % 3 === 0 && $i % 5 === 0) {
+        echo ("fizzbuzz");
+    } else if ($i % 3 === 0) {
+        echo ("fizz");
+    } else if ($i % 5 === 0) {
+        echo ("buzz");
+    } else { 
+        echo ($i);
     }
+    
    
 
    print "\n";
