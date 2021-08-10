2ec4dcd4dd8048a7dd8fb6fb3e5635bbddaac3e7
ryuki-apex <doragonknight212@gmail.com>
2021-08-10 14:47:16 +0900

    FizzBuzz test

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
-echo "github first";
+declare (strict_types=1)
+
+function fizzbuzz(int $i):string
+{
+    $result ='';
+    if ($i % 3 === 0){
+        $result .='Fizz';
+    }
+
+    if ($i % 5 === 0){
+        $result .='Buzz';
+    }
+
+    if (($i % 3 !== 0)&&($i % 5 !== 0)) {
+        $result .=(string)$i;
+    }
+
+    for ($i = 1; $i <= 100; $i++){
+        echo fizzbuzz($i),PHP_EOL;
+    }
+
+
+}
+
 ?>
