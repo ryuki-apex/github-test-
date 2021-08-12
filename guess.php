ad6ebd339dce1cd40f99fe1a6fb0de204e982813
ryuki-apex <doragonknight212@gmail.com>
2021-08-12 11:33:47 +0900

    number

--- guess.php
+++ guess.php
@@ -1,29 +1,23 @@
 <?php
 error_reporting(0);
-
-    echo ("1－100の中から数字を当ててください。");
+echo ("1－100の中から数字を当ててください。");
 //標準入力を出す。
 $s=rand (1,100);
 //ランダムに1－100で数字を生成する。
 while($num!==$s){
 $num =trim(fgets(STDIN));
-
 //1－100以外を無効にする
-
-if ($num>=1 && $num<=100){
-    
-}else {
-    echo"無効な入力です\n";
-break;
+if ($num>=101 || $num<=0){
+    echo("無効な入力です");
+     break;
 }
  //判定。
  //もし$numと$sが等しければ正解と表示
 if($num==$s){
     echo("正解です");
-   break;
+     break;
 //$numより$sが小さければ小さいと表示
 }else if ($num>$s){ // $num > 50
-    echo $num;
     echo ("もっと小さい"); 
  //$numより$sが大きければ大きいと表示
 }else if ($num<$s){ // $num < 50
